d59f3cb16d20b879251d54858d9674958dd6ca08
Yao Lu <luyaocns@gmail.com>
2017-03-30 15:11:12 +0800

    2017-03-30T15:11:12.24

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,14 @@
+if !isdir(Pkg.dir("Utils"))
+  Pkg.clone("https://github.com/AStupidBear/Utils.jl.git")
+  Pkg.build("Utils")
+end
+
+if !isdir(Pkg.dir("HyperOpt"))
+  Pkg.rm("HyperOpt")
+  Pkg.clone("https://github.com/AStupidBear/HyperOpt.jl.git")
+  Pkg.build("HyperOpt")
+end
+
 run(`pip install -I numpy==1.11.0`)
 !isdir("hyperopt-sklearn") && run(`git clone https://github.com/hyperopt/hyperopt-sklearn.git`)
 cd("hyperopt-sklearn")
